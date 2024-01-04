bbf4c0fc76285acb21f116f662efbd93b9147b87
Ivy <145228838+Ivette12345@users.noreply.github.com>
2024-01-04 17:07:15 -0600

    Add files via upload

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,18 +1,28 @@
 --1. List the companies names that failed including the US as the country.
 select company_name, outcome, country
 from campaign
-where outcome='failed' and country='US';
+where outcome='failed'and country='US';
 
---2. List the frequency counts in descending order of company names with successful outcome.
-select company_name, count(outcome)
-from campaign
+
+--2. List the frequency counts in descending order of categories with successful outcome.
+select category.category_id, category, count(outcome) 
+from category
+join campaign
+on category.category_id=campaign.category_id
 where outcome='successful'
-group by company_name
+group by category.category_id, outcome
 order by count(outcome)desc;
 
---3. List the contact_id for employees whose last name begins with letter B along with company_name
+--3. List the contact_id forcontacts whose last name begins with letter B along with company_name
 select contacts.contact_id, contacts.last_name, campaign.company_name
 from contacts
 join campaign
 on contacts.contact_id=campaign.contact_id
 where last_name like'B%';
+
+
+
+
+
+
+
